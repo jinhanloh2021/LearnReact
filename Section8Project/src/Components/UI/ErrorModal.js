2b15b2a316e1505fff1cd28666e13462c5661601
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-06-18 13:22:42 +0800

    Added react portals.

--- Section8Project/src/Components/UI/ErrorModal.js
+++ Section8Project/src/Components/UI/ErrorModal.js
@@ -1,23 +1,45 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import classes from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
 
+//we can have multiple components inside a single file, but only export a single component.
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 export default function ErrorModal(props) {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </>
   );
 }
