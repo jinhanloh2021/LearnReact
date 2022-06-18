4460400c7370029bc406b39c692a89742e6b259e
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-06-18 14:35:42 +0800

    Changed from using state to using react refs. This makes it run faster as I don't have to constantly poll the state of the input. But it makes the input element uncontrolled, as I'm manually setting the input value to empty string.

--- Section8Project/src/Components/Users/AddUser.js
+++ Section8Project/src/Components/Users/AddUser.js
@@ -1,46 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 
 export default function AddUser(props) {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
+  const nameInputRef = useRef(); //connect to an element using prop ref=nameInputRef //return type is a real DOM element
+  const ageInputRef = useRef(); //contains { current: DOMObject } Read using ageInputRef.current.value
+
+  //called when form submitted
   const addUserHandler = (event) => {
-    event.preventDefault();
-    if (!enteredUsername.trim() || !enteredAge.trim()) {
+    event.preventDefault(); //prevents calling the HTTP post method
+    const enteredName = nameInputRef.current.value; //fetching data using refs
+    const enteredUserAge = ageInputRef.current.value; //these two inputs are uncontrolled components, because their internal state is not controlled by react.
+    if (!enteredName.trim() || !enteredUserAge.trim()) {
+      //error state depends on the value of username and age ONSUBMIT, not onChange.
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values)',
       });
       return;
     }
-    if (+enteredAge < 0) {
+    if (+enteredUserAge < 0) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (greater than 0)',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge); // Lifting the state up to App.js
-
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    props.onAddUser(enteredName, enteredUserAge); // Lifting the state up to App.js. userList state in App.js
+    nameInputRef.current.value = ''; //not using React. Abit hacky. This is uncontrolled component.
+    ageInputRef.current.value = '';
   };
 
   const errorHandler = () => {
-    setError(null);
+    setError(null); //DOM re-rerenders and the error overlay and modal disappears.
   };
 
   return (
@@ -55,19 +51,9 @@ export default function AddUser(props) {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
