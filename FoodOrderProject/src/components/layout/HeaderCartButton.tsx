bcdd62a1eb61e5676bba1be2dfd1576077346577
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2022-06-27 15:32:13 +0800

    Finish Food Order project. Add Animation when items in cart changes.

--- FoodOrderProject/src/components/layout/HeaderCartButton.tsx
+++ FoodOrderProject/src/components/layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './HeaderCartButton.scss';
 import CartIcon from '../cart/CartIcon';
 import CartContext from '../../store/cart-context';
@@ -9,14 +9,32 @@ type Props = {
 
 export default function HeaderCartButton({ onClick }: Props) {
   const cartContext = useContext(CartContext); //component re-rendered whenever context changes.
+  const [btnAnimate, setBtnAnimate] = useState(false);
 
   //Iterates through Item[], sums up all amounts.
   const numCartItems: number = cartContext.items.reduce((currentNum, item) => {
     return currentNum + (item.amount || 0); //item can be undefined
   }, 0);
 
+  //By using useEffect, React will do something after rendering.
+  let buttonClassName = `header-cart-button ${btnAnimate ? 'bump' : ''}`;
+  useEffect(() => {
+    if (cartContext.totalAmount === 0) {
+      return;
+    }
+    setBtnAnimate(true);
+    const timer = setTimeout(() => {
+      setBtnAnimate(false);
+    }, 300);
+
+    //return cleanup function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.totalAmount]);
+
   return (
-    <button className="header-cart-button" onClick={onClick}>
+    <button className={buttonClassName} onClick={onClick}>
       <span className="header-icon">
         <CartIcon />
       </span>
