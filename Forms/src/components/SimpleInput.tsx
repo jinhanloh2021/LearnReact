92dbb404b308bcc2b5ab24a460b8be8c5831a4e3
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-07-11 09:50:54 +0800

    Start new project Forms.

---
+++ Forms/src/components/SimpleInput.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function SimpleInput() {
+  return (
+    <form>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+}
